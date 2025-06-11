1ee3d995b439757eed6338455386984b526ae86c
Jorge Eyzaguirre <jorgeyza@gmail.com>
2025-06-11 17:25:52 -0500

    feat: udpate Menu component and transformer to support recursive component resolving

--- experiments/sandbox-next/src/components/menu/index.tsx
+++ experiments/sandbox-next/src/components/menu/index.tsx
@@ -1,8 +1,9 @@
 import type { ReactNode } from "react";
 
 export interface MenuItem {
-  type: 'span' | 'anchor';
-  text: string;
+  id: string;
+  contentType: string;
+  component: ReactNode;
   children?: MenuItem[];
 }
 
@@ -13,30 +14,18 @@ export interface MenuProps {
 export default function Menu({ items }: Readonly<MenuProps>) {
   const renderMenuItem = (item: MenuItem): ReactNode => {
     const children = item.children?.map((child) => renderMenuItem(child));
-    
+
     return (
-      <li key={item.text} className="ml-4">
-        {item.type === 'anchor' ? (
-          <button 
-            className="text-blue-500 hover:underline"
-          >
-            {item.text}
-          </button>
-        ) : (
-          <span>{item.text}</span>
-        )}
-        {children && children.length > 0 && (
-          <ul>{children}</ul>
-        )}
+      <li key={item.id} className="ml-4">
+        {item.component}
+        {children && children.length > 0 && <ul>{children}</ul>}
       </li>
     );
   };
 
   return (
     <nav className="w-fit p-4 border border-amber-200 rounded shadow">
-      <ul>
-        {items.map(renderMenuItem)}
-      </ul>
+      <ul>{items.map(renderMenuItem)}</ul>
     </nav>
   );
 }
--- experiments/sandbox-next/src/components/menu/menu.transformer.tsx
+++ experiments/sandbox-next/src/components/menu/menu.transformer.tsx
@@ -1,15 +1,37 @@
-import type { MenuProps } from ".";
+import { widgetRegistryInversify } from "@/globals/componentRegistry";
+import type { CMSWidget } from "@/types/cms";
+import type { MenuItem, MenuProps } from ".";
 
-export interface MenuItemFromDataSource {
-  type: 'span' | 'anchor';
-  text: string;
-  children?: MenuItemFromDataSource[];
+export default async function transformer(
+  widget: Readonly<CMSWidget>,
+  ctx: any
+): Promise<MenuProps> {
+  const transformItems = async (items: CMSWidget[]): Promise<MenuItem[]> => {
+    return Promise.all(
+      items.map(async (item) => {
+        const Component = widgetRegistryInversify.getComponent(
+          item.contentType,
+          true
+        );
+        const children = item.children
+          ? await transformItems(item.children)
+          : undefined;
+
+        return {
+          id: item.id,
+          contentType: item.contentType,
+          component: <Component key={item.id} {...item} />,
+          children,
+        };
+      })
+    );
+  };
+
+  return {
+    items: await transformItems(widget.items as CMSWidget[]),
+  };
 }
 
-export interface MenuPropsFromDataSource {
-  items: MenuItemFromDataSource[];
-}
-
-export default function transformer(props: Readonly<MenuPropsFromDataSource>): MenuProps {
-  return props;
-}
+// TODO: Evitar que haya desbordamiento de pila (stack overflow). Podría usarse trampoline?
+// TODO: Desacoplar la implementación del inversor de control (no llamar directo a widgetRegistryInversify)
+// TODO: Abstraer estructura recursiva cuando ya esté definida la manera correcta de resolver recursivamente. Tal vez una función?
